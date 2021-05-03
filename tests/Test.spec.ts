d4e92d3d920a639733624dc8ce47e0b227011564
huangtq <969791939@qq.com>
2021-05-03 19:53:00 +0800

    chore(config): 测试git actions

--- tests/Test.spec.ts
+++ tests/Test.spec.ts
@@ -5,6 +5,7 @@ test('Test.vue', async () => {
   const wrapper = mount(Test)
   expect(wrapper.html()).toContain('Unit Test Page')
   expect(wrapper.html()).toContain('count is: 0')
+  expect(wrapper.html()).toContain('count is: 0')
   await wrapper.find('button').trigger('click')
   expect(wrapper.html()).toContain('count is: 1')
 })
